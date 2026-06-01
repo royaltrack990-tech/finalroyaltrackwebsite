bab8e9019242ed9827dfcfc4b1263dee4bcbdf91
royaltrack990-tech <royal.track990@gmail.com>
2026-06-01 15:26:46 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,32 @@ export default function RootLayout({ children }) {
           `}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '922903900769966');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=922903900769966&ev=PageView&noscript=1"
+            alt="Meta Pixel"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
