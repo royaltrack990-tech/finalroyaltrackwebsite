91e11aee0c80af37a1cb4fac85b2543e5a271ea2
royaltrack990-tech <royal.track990@gmail.com>
2026-06-17 12:54:40 +0400

    Update FloatingButtons.jsx

--- app/components/FloatingButtons.jsx
+++ app/components/FloatingButtons.jsx
@@ -66,7 +66,7 @@ export default function FloatingButtons() {
       </a>
 
       {/* Call - bottom right, animated pulse + border to attract clicks */}
-      <a href="tel:+97143934374" className="call-float" title="Call Us">
+      <a href="tel:+971552824492" className="call-float" title="Call Us">
         <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2">
           <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"/>
         </svg>
